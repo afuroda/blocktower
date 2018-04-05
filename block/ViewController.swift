7b015c7effe3663fdf2fc091e5a46440b7492bcf
koki <iosgitapp@gmail.com>
2018-04-05 19:15:31 +0900

    Initial Commit

---
+++ block/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  block
+//
+//  Created by 山口航輝 on 2018/04/05.
+//  Copyright © 2018年 koki.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
